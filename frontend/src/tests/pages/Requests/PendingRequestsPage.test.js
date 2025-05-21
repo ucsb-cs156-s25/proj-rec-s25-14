16877d1d9615d80220e9bea66cab6801d98b0708
Chloe Andersen <chloeandersen@ucsb.edu>
2025-05-20 18:37:44 -0700

    removing unused variables

--- frontend/src/tests/pages/Requests/PendingRequestsPage.test.js
+++ frontend/src/tests/pages/Requests/PendingRequestsPage.test.js
@@ -8,9 +8,6 @@ import axios from "axios";
 import AxiosMockAdapter from "axios-mock-adapter";
 import { recommendationRequestFixtures } from "fixtures/recommendationRequestFixtures";
 
-const axiosMock = new AxiosMockAdapter(axios);
-const queryClient = new QueryClient();
-
 describe("PendingRequestsPage tests", () => {
   const axiosMock = new AxiosMockAdapter(axios);
   const queryClient = new QueryClient();
